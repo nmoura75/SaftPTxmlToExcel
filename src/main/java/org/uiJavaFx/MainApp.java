a5bd3ea61ecf2be426de3d320ac19f329a16abf3
nmoura <nmoura@sapo.pt>
2025-01-28 07:06:08 +0000

    add a browse option to search for file

--- src/main/java/org/uiJavaFx/MainApp.java
+++ src/main/java/org/uiJavaFx/MainApp.java
@@ -1,6 +1,7 @@
 package org.uiJavaFx;
 
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -8,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import org.xmlParseExcel.Constants;
+
+import java.io.File;
 
 public class MainApp extends Application {
 
@@ -17,15 +20,24 @@ public class MainApp extends Application {
         TextField textField = new TextField();
         textField.setText(Constants.XML_FILE_PATH); // Set initial value
 
-        Button button = new Button("Update Path");
-        button.setOnAction(e -> {
+        Button updateButton = new Button("Update Path");
+        updateButton.setOnAction(e -> {
             Constants.XML_FILE_PATH = textField.getText();
             System.out.println("Updated XML_FILE_PATH: " + Constants.XML_FILE_PATH);
         });
 
-        VBox root = new VBox(10, label, textField, button);
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
 
+        Button browseButton = new Button("Browse...");
+        browseButton.setOnAction(e -> {
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile != null) {
+                textField.setText(selectedFile.getAbsolutePath());
+            }
+        });
 
+        VBox root = new VBox(10, label, textField, browseButton, updateButton);
 
         Scene scene = new Scene(root, 600, 400);
 
